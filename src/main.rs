645cfb48dd546a4bf8bb8d74565c037b15d9aef3
JacksonVirgo <jacknash42@gmail.com>
2024-11-25 18:56:15 +1000

    chore: init bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bevy::{
+    app::AppExit,
+    prelude::*,
+    window::{PrimaryWindow, WindowMode},
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "DVD Logo Game".to_string(),
+                resolution: (1024.0, 576.0).into(),
+                resizable: false,
+                mode: WindowMode::Windowed,
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .insert_resource(ClearColor(Color::srgb(0.0, 0.0, 0.0)))
+        .add_systems(Startup, spawn_camera)
+        .add_systems(Update, exit_game)
+        .run();
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
+pub fn exit_game(keyboard_input: Res<ButtonInput<KeyCode>>, mut app_exit: EventWriter<AppExit>) {
+    if keyboard_input.pressed(KeyCode::Escape) {
+        app_exit.send(AppExit::Success);
+    }
 }
